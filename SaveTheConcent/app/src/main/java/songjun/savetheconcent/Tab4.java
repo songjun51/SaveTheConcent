b574f3f8b1576b8327f8635a655f034599561643
songjun51 <swj8530@naver.com>
2015-10-14 05:34:06 +0900

    20151014_2

--- SaveTheConcent/app/src/main/java/songjun/savetheconcent/Tab4.java
+++ SaveTheConcent/app/src/main/java/songjun/savetheconcent/Tab4.java
@@ -14,6 +14,7 @@ public class Tab4 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.tab_4, container, false);
+
         return v;
     }
 }
